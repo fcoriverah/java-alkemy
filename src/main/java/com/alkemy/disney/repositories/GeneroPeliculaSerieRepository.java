e29eb1821d0fe148f24e616fafb4d0cf36412a12
Francisco Leonardo Rivera Hermosilla <francisco@inf.uct.cl>
2022-04-08 15:28:00 -0400

    Fix DTO, JsonIgnore, cambios varios

--- src/main/java/com/alkemy/disney/repositories/GeneroPeliculaSerieRepository.java
+++
@@ -1,9 +0,0 @@
-package com.alkemy.disney.repositories;
-
-import com.alkemy.disney.models.GeneroPeliculaSerie;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-
-@RepositoryRestResource
-public interface GeneroPeliculaSerieRepository extends JpaRepository<GeneroPeliculaSerie, Long> {
-}
--- src/main/java/com/alkemy/disney/repositories/PersonajePeliculaSerieRepository.java
+++
@@ -1,9 +0,0 @@
-package com.alkemy.disney.repositories;
-
-import com.alkemy.disney.models.PersonajePeliculaSerie;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-
-@RepositoryRestResource
-public interface PersonajePeliculaSerieRepository extends JpaRepository<PersonajePeliculaSerie, Long> {
-}
